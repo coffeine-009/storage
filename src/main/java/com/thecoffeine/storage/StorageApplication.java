a42927db570d560a31d53ad97aecda6a5397a44c
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-01-02 22:46:36 +0200

    Spring boot, gradle init commit.

---
+++ src/main/java/com/thecoffeine/storage/StorageApplication.java
@@ -0,0 +1,12 @@
+package com.thecoffeine.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( StorageApplication.class, args );
+    }
+}
