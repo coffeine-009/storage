4b9eb879864418168ac42a819b3c8b3fb5d2b276
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-01-12 23:30:25 +0200

    Added rest docs.

--- src/test/java/com/thecoffeine/storage/AbstractTests.java
+++ src/test/java/com/thecoffeine/storage/AbstractTests.java
@@ -1,10 +1,21 @@
 package com.thecoffeine.storage;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles( "tests" )
+@AutoConfigureRestDocs( "build/generated-snippets" )
+@AutoConfigureMockMvc
+@RunWith( SpringRunner.class )
 @SpringBootTest
-public class AbstractTests {
+public abstract class AbstractTests {
 
+    @Autowired
+    protected MockMvc mockMvc;
 }
