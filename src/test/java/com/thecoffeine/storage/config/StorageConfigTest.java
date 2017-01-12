971553e9ea612605d3634bab2d4224e82e018a10
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-01-13 00:14:34 +0200

    Added comments.

--- src/test/java/com/thecoffeine/storage/config/StorageConfigTest.java
+++ src/test/java/com/thecoffeine/storage/config/StorageConfigTest.java
@@ -11,6 +11,8 @@ package com.thecoffeine.storage.config;
 import com.github.fakemongo.Fongo;
 import com.mongodb.Mongo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
@@ -28,11 +30,26 @@ public class StorageConfigTest extends AbstractMongoConfiguration {
     @Autowired
     private Environment env;
 
+
+    /**
+     * Return the name of the database to connect to.
+     *
+     * @return must not be {@literal null}.
+     */
     @Override
     protected String getDatabaseName() {
         return env.getRequiredProperty("spring.data.mongodb.database");
     }
 
+    /**
+     * Return the {@link Mongo} instance to connect to.
+     * Annotate with {@link Bean} in case you want to expose a
+     * {@link Mongo} instance to the {@link ApplicationContext}.
+     *
+     * @return Mongo client.
+     *
+     * @throws Exception
+     */
     @Override
     public Mongo mongo() throws Exception {
         return new Fongo(getDatabaseName()).getMongo();
