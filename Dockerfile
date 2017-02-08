20b227fcebb9e7af1481ef375c725e0e78e08afd
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-02-09 00:51:30 +0200

    Configure as micro service node. Add docker integration.

--- Dockerfile
+++ Dockerfile
@@ -7,20 +7,17 @@ MAINTAINER Vitaliy Tsutsman <vitaliy.tsutsman@musician-virtuoso.com>
 #- Create app dir -#
 RUN mkdir /opt/storage
 
-#- Copy sources -#
-COPY . /opt/storage
-
 # Define dafault value of DB host
 ENV DB_HOST localhost
 
 #- Set up work dir -#
 WORKDIR /opt/storage
 
-#- Build app -#
-RUN ./gradlew clean build
-
 #- Expose port -#
 EXPOSE 8080
 
 #- Run app -#
-CMD [ "./gradlew", "bootRun" ]
+CMD [ "java", "-jar", "/opt/storage/storage.jar" ]
+
+#- Add app jar -#
+ADD ./build/libs/storage-0.0.1-SNAPSHOT.jar /opt/storage/storage.jar
--- src/main/java/com/thecoffeine/storage/StorageApplication.java
+++ src/main/java/com/thecoffeine/storage/StorageApplication.java
@@ -10,6 +10,8 @@ package com.thecoffeine.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * Application class.
@@ -17,6 +19,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableResourceServer
 public class StorageApplication {
 
     /**
